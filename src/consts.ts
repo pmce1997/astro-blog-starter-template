5e8bc410c9d1f616c87c636c8ffd0decfdcd2c45
pmce1997 <pmce1997@icloud.com>
2026-06-08 13:49:18 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Peter McEvoy";
+export const SITE_DESCRIPTION = "All about me, my life so far, various thoughts and images, and hopes for the future.";
